f3a9f1b2ce541cac61522388aef4ab5204414f0d
Ailton Aires <ailtonaires5@gmail.com>
2024-05-27 19:34:17 -0300

    Adicionando import NoticiaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CepModule } from './cep/cep.module';
+import { NoticiaController } from './noticia/noticia.controller';
+import { NoticiaService } from './noticia/noticia.service';
+import { NoticiaModule } from './noticia/noticia.module';
 
 @Module({
-  imports: [CepModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CepModule, NoticiaModule],
+  controllers: [AppController, NoticiaController],
+  providers: [AppService, NoticiaService],
 })
 export class AppModule {}
