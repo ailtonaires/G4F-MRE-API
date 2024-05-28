508103528da03377120f90983494c0538d741d08
Ailton Aires <ailtonaires5@gmail.com>
2024-05-28 18:31:17 -0300

    Instanciando AllExceptionsFilter na main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {AllExceptionsFilter} from './interceptor/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors({
     origin: 'http://localhost:3000',
   });
