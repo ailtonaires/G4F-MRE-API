24ca1849ded89e05868e724eab05139d68cd8b0e
Ailton Aires <ailtonaires5@gmail.com>
2024-05-29 10:34:07 -0300

    Adicionando decorators do swagger

--- src/cep/cep.controller.ts
+++ src/cep/cep.controller.ts
@@ -3,16 +3,16 @@ import { CepService } from './cep.service';
 import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
 
 
-@ApiTags('cep') // Tag para agrupar os endpoints relacionados a 'cep'
+@ApiTags('cep') 
 @Controller('cep')
 export class CepController {
   constructor(private readonly cepService: CepService) {}
 
   @Get(':cep')
-  @ApiOperation({ summary: 'Obter informações de um CEP' }) // Descrição da operação
-  @ApiResponse({ status: 200, description: 'Informações do CEP retornadas com sucesso.' }) // Resposta esperada
-  @ApiResponse({ status: 404, description: 'CEP não encontrado.' }) // Possível resposta de erro
-  @ApiParam({ name: 'cep', required: true, description: 'O CEP a ser consultado' }) // Parâmetro da URL
+  @ApiOperation({ summary: 'Obter informações de um CEP' })
+  @ApiResponse({ status: 200, description: 'Informações do CEP retornadas com sucesso.' }) 
+  @ApiResponse({ status: 404, description: 'CEP não encontrado.' }) 
+  @ApiParam({ name: 'cep', required: true, description: 'O CEP a ser consultado' }) 
   async getCep(@Param('cep') cep: string) {
     return this.cepService.getCep(cep);
   }
